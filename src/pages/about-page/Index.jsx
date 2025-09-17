c570bf3f8622e3f23a06c188ffd1868b8bcaf104
gvnrds Windows Desktop <gvnrx@users.noreply.github.com>
2025-09-17 02:10:17 -0400

    Aded gh-pages

--- src/pages/about-page/Index.jsx
+++ src/pages/about-page/Index.jsx
@@ -1,8 +1,10 @@
 import styles from "./Index.module.css";
+import Header from "@shared/components/header/Index";
 
 export default function AboutPage() {
   return (
     <>
+      <Header />
       <h1>About Page</h1>
     </>
   );
--- src/pages/home-page/Index.jsx
+++ src/pages/home-page/Index.jsx
@@ -1,8 +1,10 @@
 import styles from "./Index.module.css";
+import Header from "@shared/components/header/Index";
 
 export default function HomePage() {
   return (
     <>
+      <Header />
       <h1>Hone Page</h1>
     </>
   );

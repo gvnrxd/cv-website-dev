b734c0bb67254792fe0bf6459da75e3f521eaff2
gvnrds Windows Desktop <gvnrx@users.noreply.github.com>
2025-09-20 14:44:01 -0400

    Added Feat: Added content to Home page   - Added basic information to rest of home page (missing styling)

--- src/pages/home-page/Index.jsx
+++ src/pages/home-page/Index.jsx
@@ -3,13 +3,15 @@ import Header from "@shared/components/header/Index";
 import SelfImprovementIcon from "@/assets/icons/SelfImprovementIcon.jsx";
 import DownArrow from "@/assets/icons/DownArrow.jsx";
 
+import Headshot from "/public/img/shared/test.jpg";
+
 export default function HomePage() {
   return (
     <>
       <section className={styles.heroSection}>
         <div className={styles.container}>
           <p className={styles.kicker}>
-            <SelfImprovementIcon size={36} color={"var(--text)"} />
+            <SelfImprovementIcon size={36} color={"var(--tea-green)"} />
             Judgement-free consulting
           </p>
           <div className={styles.body}>
@@ -43,7 +45,7 @@ export default function HomePage() {
       <section className={styles.mainContent}>
         <div style={{ backgroundColor: "var(--surface)" }}>
           <DownArrow
-            color={"var(--bg)"}
+            color={"var(--gunmetal)"}
             size={64}
             className={styles.downArrow}
           />
@@ -52,13 +54,66 @@ export default function HomePage() {
           <svg viewBox="0 0 1440 120" preserveAspectRatio="none">
             <path
               d="M0,80 C240,140 480,20 720,60 C960,100 1200,40 1440,80 L1440,0 L0,0 Z"
-              fill="var(--surface)"
+              fill="var(--surface) "
+              z-index="-1"
             />
           </svg>
         </div>
         <div className={styles.mainText}>
-          <h2>Grow Yourself</h2>
-          <p>Reach your full potential with our expert consulting services.</p>
+          <div>
+            <h2>Grow Yourself</h2>
+            <p>
+              Reach your full potential with our expert consulting services.
+            </p>
+            <button>Get started</button>
+          </div>
+          <div>
+            <div>
+              <div>
+                <h3>About Me</h3>
+                <img
+                  src={Headshot}
+                  style={{
+                    borderRadius: "50%",
+                    height: "200px",
+                    width: "200px",
+                  }}
+                />
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Tempore, officiis! Quo maiores repellat, inventore error
+                  reiciendis explicabo alias, a iste excepturi architecto
+                  molestias soluta voluptates aspernatur nam nesciunt. Quas,
+                  possimus!
+                </p>
+                <ul>
+                  <li>200+ coached</li>
+                  <li>5 years</li>
+                  <li>FAU News</li>
+                </ul>
+              </div>
+            </div>
+            <p>Got questions?</p>
+            <h2>Frequently Asked Questions</h2>
+          </div>
+          <span>Still Not Sure?</span>
+          <div>
+            <h2>What My Clients Say</h2>
+            <div>
+              <div>
+                <p>IMG</p>
+              </div>
+              <div>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Laborum, vel consequuntur. Quibusdam itaque natus veniam quasi
+                  aperiam fuga labore necessitatibus.
+                </p>
+                <p>J.Patel</p>
+                <span>Student</span>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
